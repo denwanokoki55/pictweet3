33cfe8f49ab0ef81ae19c7ed47fbb30a2440a097
Qun Lin <linkiko55@gmail.com>
2017-06-17 12:51:19 +0900

    create my page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
-end
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root 'tweets#index'
+#   get 'tweets' => 'tweets#index'
+#   get 'tweets/new' => 'tweets#new'
+#   post 'tweets' => 'tweets#create'
+#   get 'user/:id' => 'user#show' ← Sがない！！！！
+# end
+
+  Rails.application.routes.draw do
+    devise_for :users
+    root  'tweets#index'
+    get   'tweets'      =>  'tweets#index'
+    get   'tweets/new'  =>  'tweets#new'
+    post  'tweets'      =>  'tweets#create'
+    get   'users/:id'   =>  'users#show'    #Mypageへのルーティング
+  end
